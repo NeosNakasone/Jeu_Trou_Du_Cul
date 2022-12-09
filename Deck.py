92f8fe838a4d145e0e965769fa72aa8599e18481
inadias <43502822+inadias@users.noreply.github.com>
2022-12-09 16:49:00 +0100

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -1,21 +1,21 @@
 import random
-
-
-# from Player import Player
-
 class Deck():
 
     def __init__(self):
         pass
-
-    # liste des carts dans 2 tableaux
 
     symbol_of_cards = ["♥", "♦", "♣", "♠"]
     cards_type = ["2", "AS", "ROI", "DAME", "VALET", "10", "9", "8", "7", "6", "5", "4", "3"]
 
     value = 13
     element = []
-
+    """
+    @get_element
+     Etape : 1 =>Recupération des données depuis les deux lists symbole_of_cards et cards_type et les fusionner 
+     pour façon à obtenir les les 52 cartes du jeu
+     Etape : 2 => Melanger les cartes
+     
+    """
     def get_element(self):
         for card in self.cards_type:
             for symbol in self.symbol_of_cards:
@@ -24,19 +24,28 @@ class Deck():
         shuffled_element = random.sample(self.element, len(self.element))
         return shuffled_element
 
-    """
-       def compare_card(self,card,list):
-           if card in list:
-               card_index = list.index(card)
-               return card_index
-           else:
-               for one_card in list:
-                   if one_card ==
-           """
 
-    # methode pour melanger les carte  retourner par le methode get_element
-    def shuffle(self):
-        return random.sample(self.get_element(), len(self.get_element()))
 
-    # nb_carte = 52 # TODO CHERCHER LE NOMBRE DE CARTE
-    # nb_player = Player().nb_players() # TODO RECUPERER LE NOMBRE DE JOUEURS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
